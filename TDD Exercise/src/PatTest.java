5e740825cf9c49199ee1d149de7886883b2c6e80
James Robertson <james@jamesrobertson.org>
2013-02-14 11:11:36 -0500

    remove unused test

--- TDD Exercise/src/PatTest.java
+++ TDD Exercise/src/PatTest.java
@@ -5,15 +5,10 @@ import org.junit.Test;
 
 public class PatTest {
 
-	@Test
-	public void matchTest() {
-		assertEquals(0, Pat.pat("Hello", "Hell"));
-	}
-	
+
 	@Test
 	public void multipleMatchTest() {
-		assertEquals(2, Pat.pat("Hello", "llo"));
-		
+		assertEquals(2, Pat.pat("Hello", "llo"));	
 	}
 	
 	@Test
