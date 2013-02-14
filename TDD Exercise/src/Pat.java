c579e6904d35104f04beb552c981b2d5e0572df3
James Robertson <james@jamesrobertson.org>
2013-02-14 10:54:19 -0500

    Add gitignore

--- TDD Exercise/src/Pat.java
+++ TDD Exercise/src/Pat.java
@@ -1,11 +1,11 @@
 
-public class TestPat {
+public class Pat {
 	public static int pat (String subject, String pattern) 
 	{ 
 	// Post: if pattern is not a substring of subject, return -1 
 		//	       else return (zero-based) index where the pattern (first) 
 		//	       starts in subject 
-	 
+	 return 0;
 	} 
 
 }
