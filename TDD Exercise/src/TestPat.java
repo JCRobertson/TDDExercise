b394e636f1085b8b3e708a39f851d82abc2d6a79
James Robertson <james@jamesrobertson.org>
2013-02-14 10:47:14 -0500

    Initial Setup

---
+++ TDD Exercise/src/TestPat.java
@@ -0,0 +1,11 @@
+
+public class TestPat {
+	public static int pat (String subject, String pattern) 
+	{ 
+	// Post: if pattern is not a substring of subject, return -1 
+		//	       else return (zero-based) index where the pattern (first) 
+		//	       starts in subject 
+	 
+	} 
+
+}
